f2c00d1fc4e8bb64f589976f717d3a656dd26ae7
Theerap <aioun2539@gmail.com>
2017-04-27 11:18:21 +0700

    Update main_body.php

--- main_body.php
+++ main_body.php
@@ -1,7 +1,7 @@
 <?php
  use google\appengine\api\users\UserService;
  global $appid,$page,$title;
- $phpfile="work8_body_$page.php";
+ $phpfile="main_body_$page.php";
  if(file_exists($phpfile)){
  include($phpfile);
  }else{
@@ -10,8 +10,8 @@
 readfile($htmlfile);
  }
  if (UserService::isCurrentUserAdmin()){
- echo "<br><a href='work8.php?p=edit&file=$page.html' class='btn btn-default'>Edit</a>";
+ echo "<br><a href='main.php?p=edit&file=$page.html' class='btn btn-default'>Edit</a>";
  }
- include("work8_feedback.php");
+ include("main_feedback.php");
  }
 ?>
