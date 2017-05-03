52b0cd07279e1f43066ac4c702ceddae451eb332
Theerap <aioun2539@gmail.com>
2017-05-04 02:56:59 +0700

    Add files via upload

---
+++ pro_pi.php
@@ -0,0 +1,84 @@
+<div class="content-section">
+        <div class="container">
+            <div class="row">
+			<div class="col-md-2 col-sm-6">
+                            <div class="product-holder">
+                        <div class="product-item-2">
+                            <div class="product-thumb">
+						<?php panel_include("User","main_user.php"); ?>
+							</div>
+						</div>
+						</div>
+			</div>
+                <div class="col-md-3">
+                    <div class="product-holder">
+                        <div class="product-item-2">
+                            <div class="product-thumb">
+                                <img src="images/1.png" alt="Product Title" ">
+                            </div> <!-- /.product-thumb -->
+                            <div class="product-content overlay">
+                                <h5><a href="#">PRIMER</a></h5>
+                                <p>ไพรเมอร์ จะทำหน้าที่เตรียมผิวก่อนแต่งหน้า โดยทำหน้าที่กระชับรูขุมขน ทำให้รูขุมขนดูเล็กลง ผิวดูกระชับขึ้นทำให้การลงรองพื้นดูเนียนเรียบ ป้องกันผิวหน้าหมองคล้ำระหว่างวัน และทำให้เครื่องสำอางติดทนนานยิ่งขึ้น</p>
+                            </div> <!-- /.product-content -->
+                        </div> <!-- /.product-item-2 -->
+                        <div class="product-item-2">
+                            <div class="product-thumb">
+                                <img src="images/2.jpg" alt="Product Title">
+                            </div> <!-- /.product-thumb -->
+                            <div class="product-content overlay">
+							<h5><a href="#">EYELINER</a></h5>
+                                <p>อายไลเนอร์ (Eyeliner) เป็นเครื่องสำอางที่ใช้เขียนบริเวณรอบดวงตา เพื่อสร้างความหลากหลายให้กับดวงตา ทั้งดวงตาแบบหวาน ลึกลับ โฉบเฉียว หรือมีสเน่ห์น่าค้นหา อีกทั้งยังช่วยทำให้ดวงตาดูโตขึ้นมากกว่าปกติ ซึ่งอายไลเนอร์นี้มีประวัติการใช้งานคู่กับคุณสาวๆ มาอย่างยาวนาน นับย้อนไปตั้งแต่สมัยอียิปต์โบราณ</p> 
+                            </div> <!-- /.product-content -->
+                        </div> <!-- /.product-item-2 -->
+                        <div class="clearfix"></div>
+                    </div> <!-- /.product-holder -->
+                </div> <!-- /.col-md-5 -->
+				<div class="col-md-3">
+                    <div class="product-holder">
+                        <div class="product-item-2">
+                            <div class="product-thumb">
+                                <img src="images/3.jpg" alt="Product Title">
+                            </div> <!-- /.product-thumb -->
+                            <div class="product-content overlay">
+							<h5><a href="#">LIPSTICK</a></h5>
+                                <p>ลิปสติก คือ เครื่องสำอางที่ใช้แต่งริมฝีปาก มักเป็นสีแดงหรือสีชมพู ทำให้ริมฝีปากสวยงาม และปกปิดความบกพร่องของริมฝีปาก</p>
+                                </div> <!-- /.product-content -->
+                        </div> <!-- /.product-item-2 -->
+                        <div class="product-item-2">
+                            <div class="product-thumb">
+                                <img src="images/4.png" alt="Product Title">
+                            </div> <!-- /.product-thumb -->
+                            <div class="product-content overlay">
+							<h5><a href="#">BLUSH</a></h5>
+                                <p>บลัชออน คือเครื่องสำอางที่ช่วยเพิ่มสีสันบริเวณแก้ม และยังช่วยสร้างมิติของใบหน้าให้ดูเล็กลงหรือเรียวงามขึ้น นอกจากนี้ยังทำให้ใบหน้าดูนวลเนียน ทำให้สีสันบนใบหน้าดูเด่นชัดขึ้น</p>
+                               </div> <!-- /.product-content -->
+                        </div> <!-- /.product-item-2 -->
+                        <div class="clearfix"></div>
+                    </div> <!-- /.product-holder -->
+                </div> <!-- /.col-md-5 -->
+				<div class="col-md-3">
+                    <div class="product-holder">
+                        <div class="product-item-2">
+                            <div class="product-thumb">
+                                <img src="images/5.jpg" alt="Product Title">
+                            </div> <!-- /.product-thumb -->
+                            <div class="product-content overlay">
+                                <h5><a href="#">EYESHADOW</a></h5>
+                                <p>เครื่องสำอางค์ที่ใช้ปัดทาบนเปลือกตาใต้คิ้ว เพื่อให้เกิดมิติความลึกของตาในแต่ละข้าง นอกจากนั้นแล้วยังทำให้ดูแล้วเด่น มีเสน่ห์อีกด้วย อายแชโดว์ มีหลายชนิด และมีสีสันต่างๆ มากมาย โดยปกติแล้วจะทำมาจากแป้ง และไมก้า ในบางครั้งอาจจะพบอายแชโดว์ในรูปของเหลว ดินสอ ได้เช่นกัน</p>
+                            </div> <!-- /.product-content -->
+                        </div> <!-- /.product-item-2 -->
+                        <div class="product-item-2">
+                            <div class="product-thumb">
+                                <img src="images/6.jpg" alt="Product Title">
+                            </div> <!-- /.product-thumb -->
+                            <div class="product-content overlay">
+                                <h5><a href="#">POWDER</a></h5>
+                                <p>แป้งฝุ่น: คือ แป้งชนิดที่มีเนื้อละเอียดบางเบาเหมือนฝุ่นละอองเล็กๆ เหมาะสำหรับทาทับลงบนผิวหน้าหลังจากการรองพื้น หรือทาคอนซีลเลอร์ไว้ แป้งฝุ่นมีคุณสมบัติ ช่วยให้ผิวดูนวลเนียนเป็นธรรมชาติยิ่งขึ้น และช่วยดูดซับความมันหลังการแต่งหน้าได้เป็นอย่างดี ควรใช้แป้งฝุ่นสีเดียวกันกับผิวเรา </p>
+                            </div> <!-- /.product-content -->
+                        </div> <!-- /.product-item-2 -->
+                        <div class="clearfix"></div>
+                    </div> <!-- /.product-holder -->
+                </div> <!-- /.col-md-5 -->
+            </div> <!-- /.row -->
+        </div> <!-- /.container -->
+    </div> <!-- /.content-section -->
