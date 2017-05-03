8282872f1856b051c772112195052c033c4961ae
Jarucha <bowwyx@gmail.com>
2017-05-04 01:24:32 +0700

    Update main.php

--- main.php
+++ main.php
@@ -130,10 +130,10 @@ href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jq
                                 <img src="images/1.png" alt="Product Title" ">
                             </div> <!-- /.product-thumb -->
                             <div class="product-content overlay">
-                                <h5><a href="#">Kooler Shirt</a></h5>
+                                <h5><a href="#">PRIMER</a></h5>
                                 <span class="tagline">Partner Name</span>
                                 <span class="price">$30.00</span>
-                                <p>Doloremque quo possimus quas necessitatibus blanditiis excepturi. Commodi, sunt tenetur deleniti labore!</p>
+                                <p>ไพรเมอร์ จะทำหน้าที่เตรียมผิวก่อนแต่งหน้า โดยทำหน้าที่กระชับรูขุมขน ทำให้รูขุมขนดูเล็กลง ผิวดูกระชับขึ้นทำให้การลงรองพื้นดูเนียนเรียบ ป้องกันผิวหน้าหมองคล้ำระหว่างวัน และทำให้เครื่องสำอางติดทนนานยิ่งขึ้น</p>
                             </div> <!-- /.product-content -->
                         </div> <!-- /.product-item-2 -->
                         <div class="product-item-2">
@@ -141,10 +141,10 @@ href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jq
                                 <img src="images/2.jpg" alt="Product Title">
                             </div> <!-- /.product-thumb -->
                             <div class="product-content overlay">
-                                <h5><a href="#">Koolest Shirt</a></h5>
+                                <h5><a href="#">LIPSTICK</a></h5>
                                 <span class="tagline">Partner Name</span>
                                 <span class="price">$45.00</span>
-                                <p>Doloremque quo possimus quas necessitatibus blanditiis excepturi. Commodi, sunt tenetur deleniti labore!</p>
+                                <p>ลิปสติก คือ เครื่องสำอางที่ใช้แต่งริมฝีปาก มักเป็นสีแดงหรือสีชมพู ทำให้ริมฝีปากสวยงาม และปกปิดความบกพร่องของริมฝีปาก</p>
                             </div> <!-- /.product-content -->
                         </div> <!-- /.product-item-2 -->
                         <div class="clearfix"></div>
@@ -157,10 +157,10 @@ href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jq
                                 <img src="images/3.jpg" alt="Product Title">
                             </div> <!-- /.product-thumb -->
                             <div class="product-content overlay">
-                                <h5><a href="#">Kooler Shirt</a></h5>
+                                <h5><a href="#">EYESHADOW</a></h5>
                                 <span class="tagline">Partner Name</span>
                                 <span class="price">$30.00</span>
-                                <p>Doloremque quo possimus quas necessitatibus blanditiis excepturi. Commodi, sunt tenetur deleniti labore!</p>
+                                <p>เครื่องสำอางค์ที่ใช้ปัดทาบนเปลือกตาใต้คิ้ว เพื่อให้เกิดมิติความลึกของตาในแต่ละข้าง นอกจากนั้นแล้วยังทำให้ดูแล้วเด่น มีเสน่ห์อีกด้วย อายแชโดว์ มีหลายชนิด และมีสีสันต่างๆ มากมาย โดยปกติแล้วจะทำมาจากแป้ง และไมก้า ในบางครั้งอาจจะพบอายแชโดว์ในรูปของเหลว ดินสอ ได้เช่นกัน</p>
                             </div> <!-- /.product-content -->
                         </div> <!-- /.product-item-2 -->
                         <div class="product-item-2">
@@ -168,10 +168,10 @@ href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jq
                                 <img src="images/4.png" alt="Product Title">
                             </div> <!-- /.product-thumb -->
                             <div class="product-content overlay">
-                                <h5><a href="#">Koolest Shirt</a></h5>
+                                <h5><a href="#">EYELINER</a></h5>
                                 <span class="tagline">Partner Name</span>
                                 <span class="price">$45.00</span>
-                                <p>Doloremque quo possimus quas necessitatibus blanditiis excepturi. Commodi, sunt tenetur deleniti labore!</p>
+                                <p>อายไลเนอร์ (Eyeliner) เป็นเครื่องสำอางที่ใช้เขียนบริเวณรอบดวงตา เพื่อสร้างความหลากหลายให้กับดวงตา ทั้งดวงตาแบบหวาน ลึกลับ โฉบเฉียว หรือมีสเน่ห์น่าค้นหา อีกทั้งยังช่วยทำให้ดวงตาดูโตขึ้นมากกว่าปกติ ซึ่งอายไลเนอร์นี้มีประวัติการใช้งานคู่กับคุณสาวๆ มาอย่างยาวนาน นับย้อนไปตั้งแต่สมัยอียิปต์โบราณ และยุคเมโสโปเตเมียกันเลยทีเดียว</p>
                             </div> <!-- /.product-content -->
                         </div> <!-- /.product-item-2 -->
                         <div class="clearfix"></div>
@@ -184,10 +184,10 @@ href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jq
                                 <img src="images/5.jpg" alt="Product Title">
                             </div> <!-- /.product-thumb -->
                             <div class="product-content overlay">
-                                <h5><a href="#">Kooler Shirt</a></h5>
+                                <h5><a href="#">BLUSH</a></h5>
                                 <span class="tagline">Partner Name</span>
                                 <span class="price">$30.00</span>
-                                <p>Doloremque quo possimus quas necessitatibus blanditiis excepturi. Commodi, sunt tenetur deleniti labore!</p>
+                                <p>บลัชออน คือเครื่องสำอางที่ช่วยเพิ่มสีสันบริเวณแก้ม และยังช่วยสร้างมิติของใบหน้าให้ดูเล็กลงหรือเรียวงามขึ้น นอกจากนี้ยังทำให้ใบหน้าดูนวลเนียน ทำให้สีสันบนใบหน้าดูเด่นชัดขึ้น</p>
                             </div> <!-- /.product-content -->
                         </div> <!-- /.product-item-2 -->
                         <div class="product-item-2">
@@ -195,10 +195,11 @@ href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jq
                                 <img src="images/6.jpg" alt="Product Title">
                             </div> <!-- /.product-thumb -->
                             <div class="product-content overlay">
-                                <h5><a href="#">Koolest Shirt</a></h5>
+                                <h5><a href="#">POWDER</a></h5>
                                 <span class="tagline">Partner Name</span>
                                 <span class="price">$45.00</span>
-                                <p>Doloremque quo possimus quas necessitatibus blanditiis excepturi. Commodi, sunt tenetur deleniti labore!</p>
+                                <p>แป้งฝุ่น: 
+คือ แป้งชนิดที่มีเนื้อละเอียดบางเบาเหมือนฝุ่นละอองเล็กๆ เหมาะสำหรับทาทับลงบนผิวหน้าหลังจากการรองพื้น หรือทาคอนซีลเลอร์ไว้ แป้งฝุ่นมีคุณสมบัติ ช่วยให้ผิวดูนวลเนียนเป็นธรรมชาติยิ่งขึ้น และช่วยดูดซับความมันหลังการแต่งหน้าได้เป็นอย่างดี ควรใช้แป้งฝุ่นสีเดียวกันกับผิวเรา (ควรทดลองสีก่อนซื้อที่ Counter) เราใช้แป้งฝุ่นเพื่อทำให้รองพื้นติดทนนานมากขึ้นและการแต่งหน้าก็จะดูสวย สมบูรณ์แบบ </p>
                             </div> <!-- /.product-content -->
                         </div> <!-- /.product-item-2 -->
                         <div class="clearfix"></div>
@@ -207,8 +208,6 @@ href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jq
             </div> <!-- /.row -->
         </div> <!-- /.container -->
     </div> <!-- /.content-section -->
-
-
     
     <script src="js/vendor/jquery-1.10.1.min.js"></script>
     <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
@@ -216,7 +215,5 @@ href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jq
     <script src="js/bootstrap.js"></script>
     <script src="js/plugins.js"></script>
     <script src="js/main.js"></script>
-
-
 </body>
 </html>
