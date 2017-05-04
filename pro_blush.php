fa8f6496b6383c2e15ddbcb696ad6b59e917969e
Jarucha <bowwyx@gmail.com>
2017-05-04 10:24:04 +0700

    Update pro_blush.php

--- pro_blush.php
+++ pro_blush.php
@@ -14,7 +14,7 @@
                     <div class="product-holder">
                          <div class="product-item-2">
                             <div class="product-thumb">
-                                <img src="images/1.png" alt="Product Title" ">
+                                <img src="http://www.ulta.com/too-faced-peach-collection/images/sweet-peach-glow.jpg" alt="Product Title" ">
                             </div> <!-- /.product-thumb -->
                             <div class="product-content overlay">
                                 <h5><a href="#">Too Faced SWEET PEACH Peach Glow Kit</a></h5>
@@ -24,7 +24,7 @@
                         </div> <!-- /.product-item-2 -->		
 						 <div class="product-item-2">
                             <div class="product-thumb">
-                                <img src="images/1.png" alt="Product Title" ">
+                                <img src="https://kirariblog.files.wordpress.com/2016/10/jillstuart_blush_morecolors_2.jpg?w=705&h=470" alt="Product Title" ">
                             </div> <!-- /.product-thumb -->
                             <div class="product-content overlay">
                                 <h5><a href="#">JILL STUART Mix Blush Compact More Colors #17 Believe In Love</a></h5>
@@ -34,7 +34,7 @@
                         </div> <!-- /.product-item-2 -->
 						 <div class="product-item-2">
                             <div class="product-thumb">
-                                <img src="images/1.png" alt="Product Title" ">
+                                <img src="https://res.weloveshopping.com/925163/w_450,h_450,c_thumb/a6aa04df27bf04df727ecaf451d238f7/11.jpg" alt="Product Title" ">
                             </div> <!-- /.product-thumb -->
                             <div class="product-content overlay">
                                 <h5><a href="#">Mille Dazzling Rose Brusher #02 Peach Champagne 10g </a></h5>
@@ -44,7 +44,7 @@
                         </div> <!-- /.product-item-2 -->
 						 <div class="product-item-2">
                             <div class="product-thumb">
-                                <img src="images/1.png" alt="Product Title" ">
+                                <img src="https://scontent.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/11351690_258118484521541_506515688_n.jpg?ig_cache_key=MTIxMzg5MTc1ODkzODQwODk2MA%3D%3D.2.l" alt="Product Title" ">
                             </div> <!-- /.product-thumb -->
                             <div class="product-content overlay">
                                 <h5><a href="#">LANCOME Cushion Blush Subtil #031 Splash Orange 7g </a></h5>
@@ -54,7 +54,7 @@
                         </div> <!-- /.product-item-2 -->
 						 <div class="product-item-2">
                             <div class="product-thumb">
-                                <img src="images/1.png" alt="Product Title" ">
+                                <img src="http://i2.yesstyle.com/Assets/92/400/L_p0047140092.jpg" alt="Product Title" ">
                             </div> <!-- /.product-thumb -->
                             <div class="product-content overlay">
                                 <h5><a href="#">Canmake Cheek & Bronzer SPF26 PA+++ #02 </a></h5>
@@ -64,7 +64,7 @@
                         </div> <!-- /.product-item-2 -->
 						 <div class="product-item-2">
                             <div class="product-thumb">
-                                <img src="images/1.png" alt="Product Title" ">
+                                <img src="https://www.beauticool.com/product_images/58d25edbb49ec.jpg" alt="Product Title" ">
                             </div> <!-- /.product-thumb -->
                             <div class="product-content overlay">
                                 <h5><a href="#">REME BY SINCERE Cream Blush #02 Tattle 4.2g </a></h5>
@@ -74,7 +74,7 @@
                         </div> <!-- /.product-item-2 -->
 						 <div class="product-item-2">
                             <div class="product-thumb">
-                                <img src="images/1.png" alt="Product Title" ">
+                                <img src="http://cloudacos.com/sites/9359/files/s/products/o_1b9d2d53emo4c21505m3gh1vf.jpg" alt="Product Title" ">
                             </div> <!-- /.product-thumb -->
                             <div class="product-content overlay">
                                 <h5><a href="#">CLOUDA Pearl Blush #01 Nude </a></h5>
@@ -84,7 +84,7 @@
                         </div> <!-- /.product-item-2 -->
 						 <div class="product-item-2">
                             <div class="product-thumb">
-                                <img src="images/1.png" alt="Product Title" ">
+                                <img src="http://bandai-a.akamaihd.net/bc/img/item/xl/2335449_4.jpg" alt="Product Title" ">
                             </div> <!-- /.product-thumb -->
                             <div class="product-content overlay">
                                 <h5><a href="#">Creer Beaute Miracle Romance Cosmic Heart Cheek 4.5g</a></h5>
@@ -94,7 +94,7 @@
                         </div> <!-- /.product-item-2 -->
 						 <div class="product-item-2">
                             <div class="product-thumb">
-                                <img src="images/1.png" alt="Product Title" ">
+                                <img src="http://srv-live.lazada.co.th/p/image-82826801-8e3ac16845ac22d45c68d87a2f6d36b9-product.jpg" alt="Product Title" ">
                             </div> <!-- /.product-thumb -->
                             <div class="product-content overlay">
                                 <h5><a href="#">4U2 Matt Blusher #05 Brown Sugar 7g.</a></h5>
@@ -104,7 +104,7 @@
                         </div> <!-- /.product-item-2 -->
 						 <div class="product-item-2">
                             <div class="product-thumb">
-                                <img src="images/1.png" alt="Product Title" ">
+                                <img src="http://img.priceza.com/img/product/314/314-20170323031359-2566345651403656.jpg" alt="Product Title" ">
                             </div> <!-- /.product-thumb -->
                             <div class="product-content overlay">
                                 <h5><a href="#">EGLIPS Apple Lips Pit Blusher #08 SAND PINK 4g</a></h5>
