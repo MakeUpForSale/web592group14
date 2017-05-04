b0669d29860bebfbc46cf82967f2fba684a68c8b
Jarucha <bowwyx@gmail.com>
2017-05-04 11:56:22 +0700

    Update pro_pow.php

--- pro_pow.php
+++ pro_pow.php
@@ -12,68 +12,104 @@
 			</div>
                 <div class="col-md-3">
                     <div class="product-holder">
-                        <div class="product-item-2">
+                         <div class="product-item-2">
                             <div class="product-thumb">
-                                <img src="images/1.png" alt="Product Title" ">
+                                <img src="https://www.lauramercier.co.th/media/catalog/product/cache/145/image/dfb566a8c69bb17b492ca4175417bcaf/l/r/lr_mini-looseset2.png" alt="Product Title" ">
                             </div> <!-- /.product-thumb -->
                             <div class="product-content overlay">
-                                <h5><a href="#">PRIMER</a></h5>
-                                <p>ไพรเมอร์ จะทำหน้าที่เตรียมผิวก่อนแต่งหน้า โดยทำหน้าที่กระชับรูขุมขน ทำให้รูขุมขนดูเล็กลง ผิวดูกระชับขึ้นทำให้การลงรองพื้นดูเนียนเรียบ ป้องกันผิวหน้าหมองคล้ำระหว่างวัน และทำให้เครื่องสำอางติดทนนานยิ่งขึ้น</p>
+                                <h5><a href="#"></a>Laura Mercier Translucent Loose Setting Powder+Mini Velour Puff 9.3g</h5>
+								<span class="price">ราคา 950 บาท</span>
+                                <p>  แป้งฝุ่นเนื้อโปร่งใสไม่มีสี ที่สุดของแป้งควบคุมความมัน ขายดีอันดับ 1 ตลอดกาล </p>
+                            </div> <!-- /.product-content -->
+                        </div> <!-- /.product-item-2 -->		
+						<div class="product-item-2">
+                            <div class="product-thumb">
+                                <img src="http://dm.lnwfile.com/_/dm/_raw/jw/bv/0p.jpg" alt="Product Title" ">
+                            </div> <!-- /.product-thumb -->
+                            <div class="product-content overlay">
+                                <h5><a href="#">EGLIPS Blur Powder Pact #25</a></h5>
+								<span class="price">ราคา 590 บาท</span>
+                                <p>  โทนสีสำหรับผิวคนไทย แป้งโมเมที่ช่วยเบลอรูขุมขนและสีผิวที่ไม่สม่ำเสมอ แลดูผิวหน้าสดใส สว่างอย่างเป็นธรรมชาติ แป้งเบลอที่คุณโมเม และ Pony แนะนำ </p>
                             </div> <!-- /.product-content -->
                         </div> <!-- /.product-item-2 -->
-                        <div class="product-item-2">
+						<div class="product-item-2">
                             <div class="product-thumb">
-                                <img src="images/2.jpg" alt="Product Title">
+                                <img src="http://b.lnwfile.com/_/b/_raw/1y/7i/1e.jpg" alt="Product Title" ">
                             </div> <!-- /.product-thumb -->
                             <div class="product-content overlay">
-							<h5><a href="#">EYELINER</a></h5>
-                                <p>อายไลเนอร์ (Eyeliner) เป็นเครื่องสำอางที่ใช้เขียนบริเวณรอบดวงตา เพื่อสร้างความหลากหลายให้กับดวงตา ทั้งดวงตาแบบหวาน ลึกลับ โฉบเฉียว หรือมีสเน่ห์น่าค้นหา อีกทั้งยังช่วยทำให้ดวงตาดูโตขึ้นมากกว่าปกติ ซึ่งอายไลเนอร์นี้มีประวัติการใช้งานคู่กับคุณสาวๆ มาอย่างยาวนาน นับย้อนไปตั้งแต่สมัยอียิปต์โบราณ</p> 
+                                <h5><a href="#">Cute Press COLOR FANTASY SUN PREVENTIVE TRANSLUCENT LOOSE POWDER WITH SHIMMER 30g</a></h5>
+								<span class="price">ราคา 299 บาท</span>
+                                <p> แป้งฝุ่นผสมสารป้องกันแสงแดด เนื้อแป้งใยไหม โปร่งแสง เนียนบาง กลมกลืนเป็นธรรมชาติกับผิว  </p>
                             </div> <!-- /.product-content -->
                         </div> <!-- /.product-item-2 -->
-                        <div class="clearfix"></div>
-                    </div> <!-- /.product-holder -->
-                </div> <!-- /.col-md-5 -->
-				<div class="col-md-3">
-                    <div class="product-holder">
-                        <div class="product-item-2">
+						<div class="product-item-2">
                             <div class="product-thumb">
-                                <img src="images/3.jpg" alt="Product Title">
+                                <img src="http://cdn3.bigcommerce.com/s-0sg0m/product_images/theme_images/bnpowder.jpg?t=1447709245" alt="Product Title" ">
                             </div> <!-- /.product-thumb -->
                             <div class="product-content overlay">
-							<h5><a href="#">LIPSTICK</a></h5>
-                                <p>ลิปสติก คือ เครื่องสำอางที่ใช้แต่งริมฝีปาก มักเป็นสีแดงหรือสีชมพู ทำให้ริมฝีปากสวยงาม และปกปิดความบกพร่องของริมฝีปาก</p>
-                                </div> <!-- /.product-content -->
-                        </div> <!-- /.product-item-2 -->
-                        <div class="product-item-2">
-                            <div class="product-thumb">
-                                <img src="images/4.png" alt="Product Title">
-                            </div> <!-- /.product-thumb -->
-                            <div class="product-content overlay">
-							<h5><a href="#">BLUSH</a></h5>
-                                <p>บลัชออน คือเครื่องสำอางที่ช่วยเพิ่มสีสันบริเวณแก้ม และยังช่วยสร้างมิติของใบหน้าให้ดูเล็กลงหรือเรียวงามขึ้น นอกจากนี้ยังทำให้ใบหน้าดูนวลเนียน ทำให้สีสันบนใบหน้าดูเด่นชัดขึ้น</p>
-                               </div> <!-- /.product-content -->
-                        </div> <!-- /.product-item-2 -->
-                        <div class="clearfix"></div>
-                    </div> <!-- /.product-holder -->
-                </div> <!-- /.col-md-5 -->
-				<div class="col-md-3">
-                    <div class="product-holder">
-                        <div class="product-item-2">
-                            <div class="product-thumb">
-                                <img src="images/5.jpg" alt="Product Title">
-                            </div> <!-- /.product-thumb -->
-                            <div class="product-content overlay">
-                                <h5><a href="#">EYESHADOW</a></h5>
-                                <p>เครื่องสำอางค์ที่ใช้ปัดทาบนเปลือกตาใต้คิ้ว เพื่อให้เกิดมิติความลึกของตาในแต่ละข้าง นอกจากนั้นแล้วยังทำให้ดูแล้วเด่น มีเสน่ห์อีกด้วย อายแชโดว์ มีหลายชนิด และมีสีสันต่างๆ มากมาย โดยปกติแล้วจะทำมาจากแป้ง และไมก้า ในบางครั้งอาจจะพบอายแชโดว์ในรูปของเหลว ดินสอ ได้เช่นกัน</p>
+                                <h5><a href="#">Ben Nye Fair Translucent Face Powder 42g</a></h5>
+								<span class="price">ราคา 750 บาท</span>
+                                <p>  แป้งฝุ่นเบนนาย โปร่งแสงเนื้อแมท บางเบา แต่ปกปิดได้อย่างดีเยี่ยม คุมมันเป็นเลิศ </p>
                             </div> <!-- /.product-content -->
                         </div> <!-- /.product-item-2 -->
-                        <div class="product-item-2">
+						<div class="product-item-2">
                             <div class="product-thumb">
-                                <img src="images/6.jpg" alt="Product Title">
+                                <img src="https://store.weloveshopping.com/upload/gallery2/919038/1/38/f47239baacfd7cac842a966527196381.jpg" alt="Product Title" ">
                             </div> <!-- /.product-thumb -->
                             <div class="product-content overlay">
-                                <h5><a href="#">POWDER</a></h5>
-                                <p>แป้งฝุ่น: คือ แป้งชนิดที่มีเนื้อละเอียดบางเบาเหมือนฝุ่นละอองเล็กๆ เหมาะสำหรับทาทับลงบนผิวหน้าหลังจากการรองพื้น หรือทาคอนซีลเลอร์ไว้ แป้งฝุ่นมีคุณสมบัติ ช่วยให้ผิวดูนวลเนียนเป็นธรรมชาติยิ่งขึ้น และช่วยดูดซับความมันหลังการแต่งหน้าได้เป็นอย่างดี ควรใช้แป้งฝุ่นสีเดียวกันกับผิวเรา </p>
+                                <h5><a href="#">Rimmel Stay Matte Pressed Powder No.001 #Transparent 14g</a></h5>
+								<span class="price">ราคา 1100 บาท</span>
+                                <p>  แป้งฝุ่นอัดแข็ง เนื้อแมท คุมมันยาวนาน สำหรับผิวมัน-ธรรมดา </p>
+                            </div> <!-- /.product-content -->
+                        </div> <!-- /.product-item-2 -->
+						<div class="product-item-2">
+                            <div class="product-thumb">
+                                <img src="https://static-id.lximg.com/images/pictures/12104/closeup_6d27fda9e83e987befc3f733bbff11000283a4ed_1478498651_3943_Bourjois_WEB.jpg" alt="Product Title" ">
+                            </div> <!-- /.product-thumb -->
+                            <div class="product-content overlay">
+                                <h5><a href="#">Bourjois Compact Powder #73 Miel Dore 9.5g</a></h5>
+								<span class="price">ราคา 429  บาท</span>
+                                <p>  แป้งฝุ่นอัดแข็ง เนื้อบางเบา ลดความมันวาวของผิว ปกปิดรูขุมขนให้เนียน ตลับขนาดพกพาได้สะดวก </p>
+                            </div> <!-- /.product-content -->
+                        </div> <!-- /.product-item-2 -->
+						<div class="product-item-2">
+                            <div class="product-thumb">
+                                <img src="https://scontent.cdninstagram.com/t/s320x320/14716558_1600200973339246_5119919321153273856_n.jpg" alt="Product Title" ">
+                            </div> <!-- /.product-thumb -->
+                            <div class="product-content overlay">
+                                <h5><a href="#">PALLADIO Rice Powder #Warm Beige 17g</a></h5>
+								<span class="price">ราคา 590 บาท</span>
+                                <p>  แป้งข้าวที่ช่วยควบคุมความมันส่วนเกินได้เป็นอย่างดี</p>
+                            </div> <!-- /.product-content -->
+                        </div> <!-- /.product-item-2 -->
+						<div class="product-item-2">
+                            <div class="product-thumb">
+                                <img src="http://cozbeauty.com/wp-content/uploads/2017/03/etude-house-secret-beam-powder-pact-spf36pa-16g-n02-light-pearl-beige-3001-3647973-b93f5a4f0a123005468f0f8be6b9b7a9-zoom.jpg" alt="Product Title" ">
+                            </div> <!-- /.product-thumb -->
+                            <div class="product-content overlay">
+                                <h5><a href="#">Etude Secret Beam Powder Pact  natural pearl beige</a></h5>
+								<span class="price">ราคา 1195 บาท</span>
+                                <p> แป้งฝุ่นเนื้อเนียนมีประกาย ไม่ผสมรองพื้น คุมมัน สามารถเติมระหว่างวัน หรือทาหลังลงบีบี ใช้แล้วหน้าใสเปล่งประกายดุจสาวเกาหลี </p>
+                            </div> <!-- /.product-content -->
+                        </div> <!-- /.product-item-2 -->
+						<div class="product-item-2">
+                            <div class="product-thumb">
+                                <img src="http://www.thaibestsellers.com/wp-content/uploads/2015/04/Srichand-Translucent-Powder.jpg" alt="Product Title" ">
+                            </div> <!-- /.product-thumb -->
+                            <div class="product-content overlay">
+                                <h5><a href="#">Srichand Translucent Powder 10g</a></h5>
+								<span class="price">ราคา 290 บาท</span>
+                                <p> แป้งฝุ่นโปรงแสงที่มาแรงที่สุดในขณะนี้ พร้อมการันตีว่าควบคุมความมันขั้นเทพจากบิวตี้บล็อกเกอร์มากมาย  </p>
+                            </div> <!-- /.product-content -->
+                        </div> <!-- /.product-item-2 -->
+						<div class="product-item-2">
+                            <div class="product-thumb">
+                                <img src="http://www.bisousbisousthailand.com/media/catalog/product/cache/2/small_image/262x262/9df78eab33525d08d6e5fb8d27136e95/6/0/60036_set_mb_powderpact_1_1.jpg" alt="Product Title" ">
+                            </div> <!-- /.product-thumb -->
+                            <div class="product-content overlay">
+                                <h5><a href="#">BISOUS BISOUS Miracle Blooming Skin Finisher Powder Pact Super Smooth Pink 10g</a></h5>
+								<span class="price">ราคา 995 บาท</span>
+                                <p>  ช่วยให้ผิวคุณเปล่งปลั่งนุ่มนวล </p>
                             </div> <!-- /.product-content -->
                         </div> <!-- /.product-item-2 -->
                         <div class="clearfix"></div>
@@ -82,3 +118,5 @@
             </div> <!-- /.row -->
         </div> <!-- /.container -->
     </div> <!-- /.content-section -->
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy Security Status Help
